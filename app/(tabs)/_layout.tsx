9f88aaa86b97f3ce6a0b0dbfaea1b4e9fea7c59e
Apple <knvkhera30@gmail.com>
2025-12-18 00:01:08 +0530

    feat : Version 1 , Woking for both android and ios

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <Ionicons name="home-outline" size={28} color={color} />,
